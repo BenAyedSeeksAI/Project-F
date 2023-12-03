db2bd75ce7a464ab687917ba03f9a3553b24db03
Mohamed Fares Ben Ayed <faresaction@yahoo.fr>
2023-12-03 11:58:53 +0100

    [FEAT]: Add candidate deletion

--- Controller/CandidateController.go
+++ Controller/CandidateController.go
@@ -28,11 +28,30 @@ func InsertCandidate(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Candidate inserted successfully"})
 }
+func DeleteCandidate(c *gin.Context) {
+	dbse, exists := c.Get("db")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not available"})
+	}
+	var data map[string]interface{}
+	if err := c.BindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse JSON body"})
+		return
+	}
+	candId := uint(int(data["cand_id"].(float64)))
 
+	err := models.DBDeleteCandidate(dbse.(*sql.DB), candId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete candidate"})
+		return
+	}
+	c.JSON(http.StatusAccepted, gin.H{"message": "Candidate: Successfull deletion!"})
+}
 func GetCandidateDetails(c *gin.Context) {
 	dbse, exists := c.Get("db")
 	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection not available"})
+		return
 	}
 	candidate, err := models.DBGetAllCandidates(dbse.(*sql.DB))
 	if err != nil {
